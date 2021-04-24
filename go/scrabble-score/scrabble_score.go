1503fe0ac0982b4d8ecc97ddefe4944c6bc8a178
Sami Harju <sami.1.harju@nokia.com>
2021-04-24 13:06:03 +0300

    Optimization with map

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -3,32 +3,21 @@ package scrabble
 
 import "strings"
 
-type point struct {
-	letters string
-	value   int
-}
-
-var points = []point{
-	{"aeioulnrst", 1},
-	{"dg", 2},
-	{"bcmp", 3},
-	{"fhvwy", 4},
-	{"k", 5},
-	{"jx", 8},
-	{"qz", 10},
+var letterScore = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
 }
 
 // Returns total score for formed word.
 func Score(s string) int {
 	score := 0
-	for _, c := range strings.ToLower(s) {
-		for _, point := range points {
-			count := strings.Count(point.letters, string(c))
-			if count != 0 {
-				score += count * point.value
-				break
-			}
-		}
+	for _, c := range strings.ToUpper(s) {
+		score += letterScore[c]
 	}
 	return score
 }
