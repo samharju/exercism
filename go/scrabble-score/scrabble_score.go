303b14bf1cc939bd27185d6ffefbd9607a06f560
Sami Harju <sami.1.harju@nokia.com>
2021-04-24 13:13:35 +0300

    Even more optimization with switch

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -3,21 +3,32 @@ package scrabble
 
 import "strings"
 
-var letterScore = map[rune]int{
-	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
-	'D': 2, 'G': 2,
-	'B': 3, 'C': 3, 'M': 3, 'P': 3,
-	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-	'K': 5,
-	'J': 8, 'X': 8,
-	'Q': 10, 'Z': 10,
-}
-
 // Returns total score for formed word.
 func Score(s string) int {
 	score := 0
 	for _, c := range strings.ToUpper(s) {
-		score += letterScore[c]
+		switch c {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score++
+
+		case 'D', 'G':
+			score += 2
+
+		case 'B', 'C', 'M', 'P':
+			score += 3
+
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+
+		case 'K':
+			score += 5
+
+		case 'J', 'X':
+			score += 8
+
+		case 'Q', 'Z':
+			score += 10
+		}
 	}
 	return score
 }
