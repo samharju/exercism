0d4e191b16aa69db79ac6dc5495bd7994748619d
Sami Harju <sami.harju@gmail.com>
2022-03-16 20:20:24 +0000

    rust stuff

---
+++ rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,29 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+    Debug,
+}
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    let lvl = format!("{:?}", level);
+    format!("[{}]: {}", lvl.to_uppercase(), message)
+}
+pub fn info(message: &str) -> String {
+    log(LogLevel::Info, message)
+}
+pub fn warn(message: &str) -> String {
+    log(LogLevel::Warning, message)
+}
+pub fn error(message: &str) -> String {
+    log(LogLevel::Error, message)
+}
+pub fn debug(message: &str) -> String {
+    log(LogLevel::Debug, message)
+}
